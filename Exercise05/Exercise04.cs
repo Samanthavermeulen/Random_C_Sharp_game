80ed05ce272d48760614d3988397b27e47a68f67
Samanthavermeulen <samantha.vermeulen@hotmail.com>
2019-04-12 10:35:33 +0200

    add random game

---
+++ Exercise05/Exercise04.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+namespace Exercise05
+{
+    public class Exercise04
+    {
+        public void randonNummer()
+        {
+            var random = new Random();
+            var nexRandom = random.Next(1, 10);
+            
+
+            for (int i = 0; i < 4; i++)
+            {
+                var user = Convert.ToInt32(Console.ReadLine());
+
+
+                if (i == 0)
+                {
+                    Console.WriteLine("First Attempt");
+                    if (nexRandom == user)
+                    {
+
+                        Console.WriteLine("You win");
+                        break;
+                    }
+
+                    Console.WriteLine("You lose");
+                }
+
+                if (i == 1)
+                {
+                    Console.WriteLine("Second Attempt");
+                    if (nexRandom == user)
+                    {
+
+                        Console.WriteLine("You win");
+                        break;
+                    }
+
+                    Console.WriteLine("You lose");
+                }
+
+                if (i == 2)
+                {
+                    Console.WriteLine("Third Attempt");
+                    if (nexRandom == user)
+                    {
+
+                        Console.WriteLine("You win");
+                        break;
+                    }
+
+                    Console.WriteLine("You lose");
+                }
+
+
+
+            }
+
+        }
+            
+        }
+    }
+
---
+++ Exercise05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Quess the number between 1 to 10");
+
+            new Exercise04().randonNummer();
+
+        }
+
+        
+    }
+}
